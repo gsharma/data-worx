8787b571b1a10c83e57a33dffec692895ddc3618
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2019-06-20 23:56:41 -0700

    Some more work on zkclient.

--- src/main/java/com/github/dataworx/zk/ZookeeperClient.java
+++ src/main/java/com/github/dataworx/zk/ZookeeperClient.java
@@ -1,6 +1,7 @@
 package com.github.dataworx.zk;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.admin.ZooKeeperAdmin;
@@ -17,6 +18,31 @@ public final class ZookeeperClient extends ZooKeeperAdmin {
   public ZookeeperClient(String connectString, int sessionTimeout, Watcher watcher,
       ZKClientConfig conf) throws IOException {
     super(connectString, sessionTimeout, watcher, conf);
+  }
+
+  @Override
+  public List<String> getChildWatches() {
+    return super.getChildWatches();
+  }
+
+
+  @Override
+  public List<String> getDataWatches() {
+    return super.getDataWatches();
+  }
+
+
+  @Override
+  public List<String> getExistWatches() {
+    return super.getExistWatches();
+  }
+
+  public long getLastZxid() {
+    return cnxn.getLastZxid();
+  }
+
+  public void disconnect() {
+    cnxn.disconnect();
   }
 
 }
